5a3b48d5e10f9b93a856f5b4bc71619a3530e4ab
Adam Diaz <adma@Adams-MacBook-Pro.local>
2019-09-26 12:51:41 -0400

    updated playground

--- Unit 1/Types & Variables.playground/Contents.swift
+++ Unit 1/Types & Variables.playground/Contents.swift
@@ -6,9 +6,38 @@ var str = "Hello, playground"
 
 // Data Types: String, Int(Integer)
 
+//==============================================
+//                String
+//==============================================
+
 // declared a variable fellowName of type String
 
 var fellowName = "Adam Diaz"
+
+print(fellowName) // David Lin
+
+fellowName = "Casandra Grullon"
+
+print(fellowName) // Casandra Grullon
+
+fellowName = "34"
+
+print(fellowName)
+
+// DOES NOT COMPILE - CANNOT ADD DIFFERENT DATA TYPES
+// let newString = "34" + 12
+
+// concatenation - adding together
+
+let fullName = "Adam" + " Diaz" // the outcome of this statement becomes Adam Diaz
+
+print("Persons full name is \(fullName)")
+
+// Using string interpolation, we are printing fullName's description
+
+let fallCycle = "iOS 6.3"
+
+print("\(fullName) is a fellow for the \(fallCycle)")
 
 // Type Inference: implicit declaration of a variable
 
@@ -16,7 +45,21 @@ var fellowName = "Adam Diaz"
 
 var cohort = "iOS 6.3"
 
-//CHARACTER
+let lowerCaseName = "adam"
+let upperCaseName = "Adam"
+
+let stringResult = lowerCaseName == upperCaseName
+print("string result is \(stringResult)")
+
+let alphabeticalOrder = "Kelby" < "Lorraine"
+
+print("alphabetical order result is \(alphabeticalOrder)")
+// the computer sees numbers as unicode
+
+//==============================================
+//                Character
+//==============================================
+
 
 var alphabet: Character = "c"
 
@@ -26,7 +69,10 @@ var alphabet: Character = "c"
 //var is mutable - meaning it can be changed
 cohort = "7.0"
 
-//INT
+//==============================================
+//               Integers
+//==============================================
+
 
 // i declared a variable called ageOfFellow, then initialized it to the nummber 32
 
@@ -38,9 +84,26 @@ futureAge
  
 // String Interpolation is embedding a variable into a String using \(variable name) syntax as seen below
 
+// integer divison truncates decimals (removes them)
+
+let integerDivison = 16 / 3
+
+print("integer divison is \(integerDivison)")
+
 print("Age of fellow 8 years from now is \(futureAge)")
 
-// DOUBLE - 62 bit
+//==============================================
+//            Division by 0
+//==============================================
+
+// if my denominator is zero, my app will crash
+
+// 89 / 0 DOES NOT COMPILE
+
+//==============================================
+//                Double - 64 Bit
+//==============================================
+
 
 // is prefered over a float
 
@@ -52,7 +115,37 @@ balance = balance + 85_000
 
 print(balance)
 
-// SHORT HAND ARITHMETIC
+// floating point divison keeps decimals
+let floatingPointDivison: Double = 16 / 3
+print("floating point division is \(floatingPointDivison)")
+
+print("integer divison is \(integerDivison)")
+
+//==============================================
+//      Floats(32 bit) vs Doubles(64 bit)
+//==============================================
+
+// you need to explictly specify if a statement is a float because the compiler will read it as a double automatically
+// double's hold, more, floats hold less
+
+let floatNum: Float = 67.990776788943
+
+let doubleNum = 67.990776788943
+
+print("float is \(floatNum) and double is \(doubleNum)")
+
+// DOES NOT COMPILE, CANNOT ADD A FLOAT WITH A DOUBLE
+// let accountBalance = floatNum + doubleNum
+
+// casting - converting a data type to another data type
+
+let accountBalance = Double(floatNum) + doubleNum
+
+print("account balance is now \(accountBalance)")
+
+//==============================================
+//            Shorthand Arithmetic
+//==============================================
 
 balance -= 180000
 
@@ -60,7 +153,10 @@ balance -= 180000
 
 print(balance)
 
-// BOOLEAN - Bool, Comparison Operators
+//==============================================
+//        Boolean: Bool, Comparison
+//==============================================
+
 
 var result = 70 < 100
 
@@ -82,7 +178,9 @@ print(otherResult)
 
 -5 > -2 && 3 >= -5
 
-// Artihmetic Operators
+//==============================================
+//         Arithematic Operators
+//==============================================
 
 // THIS DOES NOT COMPILE - LET IS IMMUTABLE
 
@@ -106,7 +204,10 @@ marathonTime /= 2
 
 print(marathonTime) // 300
 
-// MODULO OPERATOR OR REMAINDER
+//==============================================
+//        Modulo Operator or Remainder
+//==============================================
+
 
 // % - the modulos operater
 
@@ -118,8 +219,18 @@ var even = 12 % 6
 
 print(even)
 
-// CONSTANTS
+//==============================================
+//               Constants
+//==============================================
+
 
 let pi = 3.14
 
+// - (/* text */) This is known as a multi line comment, anything between the asterisks will be ignored by the code and can be read as comment
+/*
+ - Swift was introduced in 2014, it is a type safe language meaning you cant mix a declared data type with another e.g if var name = "Adam"
+ you cannot update name = 10 // won't compile type.String is not an Int.
+ - It is a modern language
+ - provides optionals that allows us to know whether a variable has a value or not.
+ */
 
